45b11c3e8e38b608ba2597810d2a67b5354e0a14
Ishant Sahni <ishantsahni222@gmail.com>
2021-06-01 20:12:39 +0530

    first commit to github emaily

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-const express = require('express');
-const app = express();
+// index.js
 
-app.get('/', (req, res) => {
-  res.send({ bye: 'buddy' });
-});
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
+
+const app = express(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
---
+++ models/User.js
@@ -0,0 +1,10 @@
+// User.js
+
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  googleId: String
+});
+
+mongoose.model('users', userSchema);
